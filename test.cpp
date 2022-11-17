5804e33bc78eb6d2f110d94f75655ceef3541bbe
GC1C15 <115126171+GC1C15@users.noreply.github.com>
2022-11-17 11:28:38 +0900

    Update test.cpp
    リモートリポジトリでコンフリクト変更

--- test.cpp
+++ test.cpp
@@ -6,7 +6,7 @@
 int main()
 {
     std::cout << "Hello World!\n";
-    std::cout << "Git clone changed\n";
+    std::cout << "Git lone changed\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
